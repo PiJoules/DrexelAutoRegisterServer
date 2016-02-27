93babf2920a0c01ab0956ae810d6510a405fea98
pijoules <lchan1994@yahoo.com>
2016-02-27 13:33:55 -0500

    added add_user route

--- app.py
+++ app.py
@@ -3,17 +3,62 @@
 
 from __future__ import print_function
 
-import flask
+import json
 
+from flask import Flask, request, jsonify
 from uuid import uuid64
 
 # Create flask obj
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.secret_key = str(uuid64())
 
 
+def response(code, message=""):
+    """Wrapper for response message."""
+    return jsonify(
+        code=code,
+        message=message
+    ), code
+
+
+def add_user(user_id, crns):
+    """Add the user and courses to the database."""
+    # TODO: add logic for adding user to db
+    pass
+
+
+###############
+# ROUTES
+##############
+
 @app.route("/")
-def index_route()
+def index_route():
+    return "Nothing to see here"
+
+
+@app.route("/add_user", methods=["POST"])
+def add_user_route():
+    """Route for adding a user ID and the crns to the database."""
+    # Get params
+    user_id = request.args.get("id", None)
+    crns = request.args.get("crns", None)
+
+    # Check params
+    if user_id is None:
+        return response(400, "'id' was not provided.")
+    if crns is None:
+        return response(400, "'crns' was not provided.")
+    try:
+        user_id = int(user_id)
+    except ValueError:
+        return response(400, "'id' must be an integer.")
+    try:
+        crns = json.loads(crns)
+    except ValueError:
+        return response(400, "'crns' must be valid json.")
+
+    add_user(user_id, crns)
+    return response(200, "Successfully added user.")
 
 
 if __name__ == "__main__":
